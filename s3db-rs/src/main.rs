8b986c7f06bce8f165c75f3d06d4f89c32222d09
jrhy <jrhy>
2021-11-21 18:36:56 -0800

    s3db-rs making a mess

--- s3db-rs/src/main.rs
+++ s3db-rs/src/main.rs
@@ -1,3 +1,40 @@
+extern crate gob;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+use serde::Deserialize;
+use gob::{Deserializer};
+//use gob::{error::ErrorKind, StreamDeserializer};
+use std::option::Option;
+
 fn main() {
     println!("Hello, world!");
 }
+
+#[derive(Debug,Deserialize)]
+struct MastRoot {
+          Link         Option<String>
+          Size         u64
+          Height       u8      
+          BranchFactor uint
+          NodeFormat   String
+}
+#[derive(Debug,Deserialize)]
+struct Root {
+    MastRoot MastRoot
+          Created      Option<String>
+          MergeSources List<String>
+          MergeMode    int        
+}
+
+#[test]
+fn goo() {
+    let buffer = include_bytes!("../1MOlCL_1nv7fJlQpPVyQnxU");
+
+    let deserializer = Deserializer::from_slice(buffer);
+    let decoded = Root::deserialize(deserializer).unwrap();
+    println!("{:?}", decoded);
+    //assert_eq!(decoded, &[]);
+}
+
+
