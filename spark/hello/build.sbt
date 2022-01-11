39b845d0d107c262418bbc9276db320becee9640
jrhy <jrhy>
2022-01-10 19:55:36 -0800

    spark/hello: try out bloop, and it is pretty great

--- spark/hello/build.sbt
+++ spark/hello/build.sbt
@@ -5,4 +5,4 @@ version := "1.0"
 scalaVersion := "2.12.15"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.0"
-
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.10" % "test"
---
+++ spark/hello/src/test/scala/SimpleAppTest.scala
@@ -0,0 +1,18 @@
+import org.scalatest.funsuite.AnyFunSuite
+import org.apache.spark.sql.{SparkSession, DataFrame}
+
+class SimpleAppTest extends AnyFunSuite {
+    test("helloSparky") {
+        val logFile = "../../go.mod"
+        val spark = SparkSession.builder
+                .appName("Simple Application Test")
+                .master("local[*]")
+                .getOrCreate
+        val logData = spark.read.textFile(logFile).cache()
+        val numAs = logData.filter(line => line.contains("a")).count()
+        val numBs = logData.filter(line => line.contains("b")).count()
+        println(s"Lines with a: $numAs, Lines with b: $numBs")
+        spark.stop()
+    }
+}
+
