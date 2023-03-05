a983321f384399b62bccf066d5b6f3f8ca3e20b3
jrhy <jrhy>
2023-03-05 14:21:40 -0800

    add fly-veloren-server

---
+++ fly-veloren-server/setup.sh
@@ -0,0 +1,5 @@
+cat <<EOF > /opt/userdata/server/server_config/description.ron
+V1("Kid-friendly. Adults welcome too. Foul language not allowed.")
+EOF
+/opt/veloren-server-cli admin add scaleTribeMember admin
+
