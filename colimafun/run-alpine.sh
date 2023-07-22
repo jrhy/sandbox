af58fb51c6baf0c4974425524c6739003c3c532c
jrhy <jrhy>
2023-07-21 21:41:47 -0700

    alpine rulez

---
+++ colimafun/run-alpine.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+if false ; then
+	if ! [ -d $HOME/.colima-debian ] ; then
+		mkdir $HOME/.colima-debian
+	fi
+	if ! [ -f $HOME/.colima-debian/.config/nvim/init.vim ] ; then
+		mkdir -p $HOME/.colima-debian/.config/nvim
+		echo 'source ~/.nvimrc' > $HOME/.colima-debian/.config/nvim/init.vim
+	fi
+	if ! [ -f $HOME/.colima-debian/.nvimrc ] ; then
+		echo 'inoremap jk <esc>' > $HOME/.colima-debian/.nvimrc
+	fi
+	cp bashrc $HOME/.colima-debian/.bashrc
+	docker build . -t colimafun:latest
+	docker run --mount type=bind,source=$HOME/.colima-debian,target=/work -ti colimafun $*
+else
+	cp -rp bashrc alpine/
+	docker build alpine -t alpinecolimafun:latest
+	docker run -ti alpinecolimafun $*
+fi
+
+
